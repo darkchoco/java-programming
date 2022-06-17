b7298cce8d1442257da2996ee20a187ad15875e2
darkchoco <darkchoco@cloudscape.kr>
2022-06-17 17:32:28 +0200

    main: [A2022-01] Difference Between map and flatMap in Java 8 - closes #4

--- stream/stream/src/main/java/StreamEx.java
+++ stream/stream/src/main/java/StreamEx.java
@@ -1,12 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamEx {
+
     public static void main( String[] args ) {
+        // map
         List<String> myList = Stream.of("Apple", "Banana")
                 .map(String::toUpperCase)
                 .collect(Collectors.toList());
         System.out.println(myList);
+
+        // flatMap
+        List<List<String>> myList1 = new ArrayList<>();
+        myList1.add(Arrays.asList("Apple", "Banana"));
+        myList1.add(Arrays.asList("Mango"));
+        System.out.println(myList1);
+        List<String> result = myList1.stream()
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+        System.out.println(result);
     }
 }
